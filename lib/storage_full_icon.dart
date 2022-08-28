13f332c6096ec378457cf221d801aac8f7ff61ab
viditjain <viditjain180@gmail.com>
2022-08-28 12:32:42 +0530

    sample app

---
+++ lib/storage_full_icon.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class StorageFullIcon extends StatefulWidget {
+  const StorageFullIcon({Key? key}) : super(key: key);
+
+  @override
+  State<StorageFullIcon> createState() => _StorageFullIconState();
+}
+
+class _StorageFullIconState extends State<StorageFullIcon> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
